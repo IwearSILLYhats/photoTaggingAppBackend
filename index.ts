9fb461b7c093db6f49253001b54957f5f23450f5
Austin <austin.moorer@gmail.com>
2025-06-26 16:41:43 -0400

    Adding placeholder routes

--- index.ts
+++ index.ts
@@ -10,6 +10,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 
+app.get("/stageselect", (req, res) => {
+  return res.json({
+    message: "Requesting list of stages and high scores for each.",
+  });
+});
+app.get("/stage/:stageid", (req, res) => {
+  return res.json({
+    message: `Requesting stageid ${req.params.stageid}`,
+  });
+});
+app.get("/guess", (req, res) => {
+  return res.json({
+    message: "Making a guess",
+  });
+});
+app.post("/score", (req, res) => {
+  return res.json({
+    message: "Posting your high score!",
+  });
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`Listening on PORT ${process.env.PORT}`);
 });
